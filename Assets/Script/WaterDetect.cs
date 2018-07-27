4b53862ebbe32666d8884376d2d48056a6ef2ab4
Unknown <dani_mrx93@hotmail.com>
2018-07-27 14:12:11 -0300

    Add water detect to frog

---
+++ Assets/Script/WaterDetect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class WaterDetect : MonoBehaviour {
+
+    public bool onWater;
+    public bool onPlatform;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Water")
+        {
+            onWater = true;
+        }
+
+        if (collision.tag == "Platform")
+        {
+            onPlatform = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Water")
+        {
+            onWater = false;
+        }
+
+        if (collision.tag == "Platform")
+        {
+            onPlatform = false;
+        }
+    }
+}
